b06c18610e5b936f37d32d355c281dfe5775fd1f
ProObito <ahmedumaid707@gmail.com>
2026-01-05 18:50:25 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,55 @@
+import { Toaster } from "@/components/ui/toaster";
+import { Toaster as Sonner } from "@/components/ui/sonner";
+import { TooltipProvider } from "@/components/ui/tooltip";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HelmetProvider } from "react-helmet-async";
+import { AuthProvider } from "@/hooks/useAuth";
 
+import Index from "./pages/Index";
+import Browse from "./pages/Browse";
+import MangaDetail from "./pages/MangaDetail";
+import Reader from "./pages/Reader";
+import Auth from "./pages/Auth";
+import Admin from "./pages/Admin";
+import NotFound from "./pages/NotFound";
+import { MangaLibrary } from "./components/MangaLibrary";
 
-h
+const queryClient = new QueryClient();
+
+const App = () => (
+  <HelmetProvider>
+    <QueryClientProvider client={queryClient}>
+      <AuthProvider>
+        <TooltipProvider>
+          <Toaster />
+          <Sonner />
+
+          <BrowserRouter>
+            <Routes>
+              <Route path="/" element={<Index />} />
+              <Route path="/browse" element={<Browse />} />
+              <Route path="/trending" element={<Browse />} />
+              <Route path="/latest" element={<Browse />} />
+              <Route path="/top-rated" element={<Browse />} />
+              <Route path="/search" element={<Browse />} />
+
+              <Route path="/library" element={<MangaLibrary />} />
+
+              <Route path="/manga/:id" element={<MangaDetail />} />
+              <Route path="/read/:mangaId/:chapterId" element={<Reader />} />
+
+              <Route path="/auth" element={<Auth />} />
+              <Route path="/login" element={<Auth />} />
+              <Route path="/admin/*" element={<Admin />} />
+
+              <Route path="*" element={<NotFound />} />
+            </Routes>
+          </BrowserRouter>
+        </TooltipProvider>
+      </AuthProvider>
+    </QueryClientProvider>
+  </HelmetProvider>
+);
+
+export default App;
